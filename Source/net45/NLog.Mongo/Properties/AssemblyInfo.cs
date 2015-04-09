42d8677a6d473be166b1dd84ca708d4e6300037b
paul.welter <pwelter@loresoft.com>
2015-04-09 11:32:35 -0500

    - restructure project to deal with packages.config issues - update .net 4.5 to driver 2.0

---
+++ Source/net45/NLog.Mongo/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NLog.Mongo")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("f41d6c78-4011-4ccf-8f05-bedb9c4fbf1a")]
