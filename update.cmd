f7a9bc601c373d3b1471ee716d1d4d5224c1b940
Paul Welter <pwelter@loresoft.com>
2015-06-18 15:37:58 -0500

    upgrade to nlog 4.0

--- update.cmd
+++ update.cmd
@@ -1,10 +1,8 @@
 @echo off
-"Source\.nuget\NuGet.exe" update "Source\NLog.Mongo\NLog.Mongo.netfx40.csproj" -r "Source\packages"
-git checkout  "Source\NLog.Mongo\packages.config"
-"Source\.nuget\NuGet.exe" update "Source\NLog.Mongo\NLog.Mongo.netfx45.csproj" -r "Source\packages"
+"Source\.nuget\NuGet.exe" update "Source\net40\NLog.Mongo\NLog.Mongo.netfx40.csproj" -r "Source\net40\packages"
+"Source\.nuget\NuGet.exe" update "Source\net45\NLog.Mongo\NLog.Mongo.netfx45.csproj" -r "Source\net45\packages"
 
-"Source\.nuget\NuGet.exe" update "Source\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx40.csproj" -r "Source\packages"
-git checkout  "Source\NLog.Mongo.ConsoleTest\packages.config"
-"Source\.nuget\NuGet.exe" update "Source\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx45.csproj" -r "Source\packages"
+"Source\.nuget\NuGet.exe" update "Source\net40\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx40.csproj" -r "Source\net40\packages"
+"Source\.nuget\NuGet.exe" update "Source\net45\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx45.csproj" -r "Source\net45\packages"
 
 msbuild master.proj -t:Refresh
