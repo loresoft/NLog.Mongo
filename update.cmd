fb2c085a46e564487b9a76638849797f83099be6
Paul Welter <pwelter@loresoft.com>
2015-07-09 09:08:11 -0500

    update packages

--- update.cmd
+++ update.cmd
@@ -1,8 +1,5 @@
 @echo off
-"Source\.nuget\NuGet.exe" update "Source\net40\NLog.Mongo\NLog.Mongo.netfx40.csproj" -r "Source\net40\packages"
-"Source\.nuget\NuGet.exe" update "Source\net45\NLog.Mongo\NLog.Mongo.netfx45.csproj" -r "Source\net45\packages"
-
-"Source\.nuget\NuGet.exe" update "Source\net40\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx40.csproj" -r "Source\net40\packages"
-"Source\.nuget\NuGet.exe" update "Source\net45\NLog.Mongo.ConsoleTest\NLog.Mongo.ConsoleTest.netfx45.csproj" -r "Source\net45\packages"
+NuGet.exe update "Source\net45\NLog.Mongo.net45.sln"
+NuGet.exe update "Source\net40\NLog.Mongo.net40.sln"
 
 msbuild master.proj -t:Refresh
