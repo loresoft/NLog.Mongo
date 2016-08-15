ddbaaba5da92350b7b11d5a1b89d06c04fc502f9
Paul Welter <pwelter@loresoft.com>
2016-08-15 09:41:40 -0500

    update build

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,5 +1,3 @@
 @echo off
-Nuget.exe restore "Source\net45\NLog.Mongo.net45.sln"
-Nuget.exe restore "Source\net40\NLog.Mongo.net40.sln"
-
+dotnet restore
 NuGet.exe install MSBuildTasks -OutputDirectory .\Tools\ -ExcludeVersion -NonInteractive
